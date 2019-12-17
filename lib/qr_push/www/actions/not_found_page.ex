2759389f931d30075bec7ecf54bc4424c9b4e0a0
Peter Saxton <peterhsaxton@gmail.com>
2019-12-17 11:23:42 +0000

    cleanup generated project

--- lib/qr_push/www/actions/not_found_page.ex
+++ lib/qr_push/www/actions/not_found_page.ex
@@ -1,12 +1,8 @@
 defmodule QrPush.WWW.Actions.NotFoundPage do
   use Raxx.SimpleServer
-  use QrPush.WWW.Layout,
-    arguments: [],
-    optional: [title: "Nothing Here"]
 
   @impl Raxx.SimpleServer
   def handle_request(_request, _state) do
     response(:not_found)
-    |> render()
   end
 end
