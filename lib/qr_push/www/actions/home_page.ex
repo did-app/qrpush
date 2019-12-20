9fcfaf02853b27cd9dd491b5d2ac3b521818590e
Peter Saxton <peterhsaxton@gmail.com>
2019-12-20 09:53:57 +0000

    configure redirect URL

--- lib/qr_push/www/actions/home_page.ex
+++ lib/qr_push/www/actions/home_page.ex
@@ -1,9 +1,15 @@
 defmodule QrPush.WWW.Actions.HomePage do
   use Raxx.SimpleServer
 
+  @web_host (if(Mix.env() == :prod) do
+               # TODO https
+               "http://www.qrpu.sh"
+             else
+               "http://localhost:5000"
+             end)
+
   @impl Raxx.SimpleServer
   def handle_request(_request = %{method: :GET}, _state) do
-    # TODO https
-    redirect("http://www.qrpu.sh")
+    redirect(@web_host)
   end
 end
