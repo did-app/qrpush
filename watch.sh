28689eb2e42e173f38f2d6b4e12165937cf66d55
Peter Saxton <peterhsaxton@gmail.com>
2020-05-29 14:19:02 +0100

    add dependencies in release

--- watch.sh
+++ watch.sh
@@ -2,6 +2,8 @@
 
 rebar3 release
 _build/default/rel/app/bin/app start
+# Foreground gives logs but inotify doesnt work to restart
+# _build/default/rel/app/bin/app foreground
 
 inotifywait -r -m -e modify -e create -e delete src |
    while read path _ file; do
